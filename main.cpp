1651f003d5a66e739b8cc6b3782347c600d0a65e
Emma <emmaneiss2@gmail.com>
2020-01-14 18:50:34 +0100

    Added main & regex for file name
    regex + string = :heart:

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <regex>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define MAP
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    regex filenameRegex("[a-z0-9_.]{1,255}.log");
+
+    string temp;    // a virer
+
+    if (argc < 2)
+    {
+#ifdef MAP
+        cerr << "man-like user manual : not written yet" << endl;
+#endif
+    }
+    else if(argc == 2)          // juste nom du fichier
+    {
+        temp = string(argv[1]);
+        if (regex_match(temp, filenameRegex))
+        {
+#ifdef MAP
+            cerr << "Correct file name" << endl;
+#endif
+        }else{
+            cerr << "Incorrect file name" << endl;
+            return -1;
+        }
+
+    }
+
 }
