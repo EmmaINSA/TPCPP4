da0bc2ff11ff751dac1008e2f76e0f29b6802fae
Emma <emmaneiss2@gmail.com>
2020-01-14 18:58:59 +0100

    Refactored regex name for future filename regex

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char** argv) {
 
     // file names with spaces and/or weird characters not accepted
-    regex filenameRegex("[a-zA-Z0-9_.]{1,255}.log");
+    regex logfilenameRegex("[a-zA-Z0-9_.]{1,255}.log");
 
     string temp;    // a virer
 
@@ -21,7 +21,7 @@ int main(int argc, char** argv) {
     else if(argc == 2)          // juste nom du fichier
     {
         temp = string(argv[1]);
-        if (regex_match(temp, filenameRegex))
+        if (regex_match(temp, logfilenameRegex))
         {
 #ifdef MAP
             cerr << "Correct file name" << endl;
